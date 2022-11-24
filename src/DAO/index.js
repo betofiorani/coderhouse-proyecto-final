ba7cd674745cd02b21106adb85fff62651e53568
Beto <alberto.fiorani@exomindset.co>
2022-11-23 21:18:17 -0300

    heroku prueba 3

--- src/DAO/index.js
+++ src/DAO/index.js
@@ -1,59 +1,62 @@
-import { environment } from "../environment/environment.js"
+//import { environment } from "../environment/environment.js"
+import ProductDaoMongo from "./product/ProductDaoMongo.js"
+import ShoppingCartDaoMongo from "./shoppingCart/ShoppingCartDaoMongo.js"
+import ChatDaoMongo from "./chat/ChatDaoMongo.js"
 
-let ProductDao
-let ShoppingCartDao
-let ChatDao
-
-switch (environment.DATABASE) {
-    case "firebase":
-        const { default: ProductDaoFirebase } = await import(
-            "./product/ProductDaoFirebase.js"
-        )
-        const { default: ShoppingCartDaoFirebase } = await import(
-            "./shoppingCart/ShoppingCartDaoFirebase.js"
-        )
-        const { default: ChatDaoFirebase } = await import(
-          "./chat/ChatDaoFirebase.js"
-        )
-
-        ProductDao = new ProductDaoFirebase()
-        ShoppingCartDao = new ShoppingCartDaoFirebase()
-        ChatDao = new ChatDaoFirebase()
-
-        break;
-
-    case "mongodb":
-        const { default: ProductDaoMongo } = await import(
-            "./product/productDaoMongo.js"
-        )
-        const { default: ShoppingCartDaoMongo } = await import(
-            "./shoppingCart/ShoppingCartDaoMongo.js"
-        )
-        const { default: ChatDaoMongo } = await import(
-          "./chat/ChatDaoMongo.js"
-        )
-
-        ProductDao = new ProductDaoMongo()
-        ShoppingCartDao = new ShoppingCartDaoMongo()
-        ChatDao = new ChatDaoMongo ()
+const ProductDao = new ProductDaoMongo()
+const ShoppingCartDao = new ShoppingCartDaoMongo()
+const ChatDao = new ChatDaoMongo ()
         
-        break;
-    case "fileSystem":
-        const { default: ProductDaoFileSystem } = await import(
-            "./product/productDaoFileSystem.js"
-        )
-        const { default: ShoppingCartDaoFileSystem } = await import(
-            "./shoppingCart/ShoppingCartDaoFileSystem.js"
-        )
-        const { default: ChatDaoFileSystem } = await import(
-          "./chat/ChatDaoFileSystem.js"
-        )
+// switch (environment.DATABASE) {
+//     case "firebase":
+//         const { default: ProductDaoFirebase } = await import(
+//             "./product/ProductDaoFirebase.js"
+//         )
+//         const { default: ShoppingCartDaoFirebase } = await import(
+//             "./shoppingCart/ShoppingCartDaoFirebase.js"
+//         )
+//         const { default: ChatDaoFirebase } = await import(
+//           "./chat/ChatDaoFirebase.js"
+//         )
 
-        ProductDao = new ProductDaoFileSystem()
-        ShoppingCartDao = new ShoppingCartDaoFileSystem()
-        ChatDao = new ChatDaoFileSystem()
+//         ProductDao = new ProductDaoFirebase()
+//         ShoppingCartDao = new ShoppingCartDaoFirebase()
+//         ChatDao = new ChatDaoFirebase()
+
+//         break;
+
+//     case "mongodb":
+//         const { default: ProductDaoMongo } = await import(
+//             "./product/productDaoMongo.js"
+//         )
+//         const { default: ShoppingCartDaoMongo } = await import(
+//             "./shoppingCart/ShoppingCartDaoMongo.js"
+//         )
+//         const { default: ChatDaoMongo } = await import(
+//           "./chat/ChatDaoMongo.js"
+//         )
+
+//         ProductDao = new ProductDaoMongo()
+//         ShoppingCartDao = new ShoppingCartDaoMongo()
+//         ChatDao = new ChatDaoMongo ()
         
-        break
-}
+//         break;
+//     case "fileSystem":
+//         const { default: ProductDaoFileSystem } = await import(
+//             "./product/productDaoFileSystem.js"
+//         )
+//         const { default: ShoppingCartDaoFileSystem } = await import(
+//             "./shoppingCart/ShoppingCartDaoFileSystem.js"
+//         )
+//         const { default: ChatDaoFileSystem } = await import(
+//           "./chat/ChatDaoFileSystem.js"
+//         )
+
+//         ProductDao = new ProductDaoFileSystem()
+//         ShoppingCartDao = new ShoppingCartDaoFileSystem()
+//         ChatDao = new ChatDaoFileSystem()
+        
+//         break
+// }
 
 export default { ProductDao, ShoppingCartDao, ChatDao }
